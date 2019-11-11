edd1b3d4eb771ecdca44f216a6148f4e726acb2e
amitlzkpa <amit.lzkpa@gmail.com>
2019-11-11 08:35:35 -0500

    work

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 require('dotenv').config(); // read .env files
 const express = require('express');
+const EqModel = require('./models/Equalizer.js');
+const bodyparser = require('bodyparser');
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+// Use middlewares
+app.use(bodyparser());
 
 // Set public folder as root
 app.use(express.static('public'));
@@ -10,10 +15,17 @@ app.use(express.static('public'));
 // Allow front-end access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
-// Redirect all traffic to index.html
-app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+// Serve homepage
+app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-// Listen for HTTP requests on port 3000
+// Add entry
+app.post('/api/add', (req, res) => {
+
+	res.send('OK');
+
+});
+
+// Listen for HTTP requests on port 4000
 app.listen(port, () => {
 	console.log('listening on %d', port);
 });
