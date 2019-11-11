ca5651b50979912379e9e153183eb3d72d052954
amitlzkpa <amit.lzkpa@gmail.com>
2019-11-11 08:01:15 -0500

    customize

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // read .env files
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Set public folder as root
 app.use(express.static('public'));
