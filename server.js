a05e9ef73b3a52063330cc2d60fefa22afc1b2b2
amitlzkpa <amit.lzkpa@gmail.com>
2019-11-11 11:40:28 -0500

    work

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const EqModel = require('./models/Equalizer.js');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Eq = require('./models/Equalizer.js');
 
+const Eq = require('./models/Equalizer.js');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
-
-let mongoURI = `mongodb://${process.env.MONGO_ID}:${process.env.MONGO_PWD}@ds127115.mlab.com:29609/lotusserver`;
+let mongoURI = `mongodb://${process.env.MONGO_ID}:${process.env.MONGO_PWD}@ds229609.mlab.com:29609/lotusserver`;
 mongoose.connect(mongoURI);
 
 app.use(express.static('public'));
@@ -21,10 +19,17 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.post('/api/add', (req, res) => {
+app.post('/api/add', async function(req, res) {
 	console.log(req.body);
 
-	res.send('OK');
+	let newEq = new Eq(req.body);
+
+	// console.log(newEq);
+
+	let savedEq = await newEq.save();
+	console.log(savedEq);
+
+	res.send(savedEq);
 
 });
 
